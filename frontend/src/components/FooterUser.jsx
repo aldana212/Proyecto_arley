918c84a8dac83744931502d1ee9b5a05ed113013
cristofertOcampo <ocampocristofert@gmail.com>
2022-12-12 18:01:55 -0500

    Puse iconos al footer y cambie cosas del estilo

--- frontend/src/components/FooterUser.jsx
+++ frontend/src/components/FooterUser.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Style from '../cssComponents/footerUser.module.css'
+import { Link } from 'react-router-dom';
 
 export function FooterUser() {
     return (
@@ -9,13 +10,13 @@ export function FooterUser() {
                 <h3>Train Station</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum eligendi aliquam maiores reiciendis iusto vitae magni cumque obcaecati dolorum quas, nulla suscipit nisi fugiat consequatur sint voluptas, doloremque dolores vero?</p>
                 <ul className={Style.socials}>
-                    <li><a href="">Facebook</a></li>
-                    <li><a href="">Twitter</a></li>
-                    <li><a href="">Instagram</a></li>
+                    <li><Link href='#'><i className='bx bxl-facebook'></i></Link></li>
+                    <li><Link href='#'><i className='bx bxl-whatsapp'></i></Link></li>
+                    <li><Link href='#'><i className='bx bxl-instagram'></i></Link></li>
                 </ul>
             </div>
             <div className={Style.footerBottom}>
-                <p>Copyright &copy;2022 TrainStation. Designed by <span>Cristofert, Daniel, Harold</span></p>
+                <p>Copyright &copy;2022 TrainStation. Designed by <span>Cristofert, Daniel, Harold.</span></p>
             </div>  
         </footer>
         </>
